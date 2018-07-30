e8d5de79f02f37c88a7d4dcf219a4b0c98465cb4
oops-accidental <smp5119@gmail.com>
2018-07-30 11:29:55 -0700

    Adding initial versions GameControl & AudioManager scripts

---
+++ Assets/Scripts/Gameplay/AudioManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour {
+
+    public static AudioManager instance;
+
+	void Start () {
+		if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Gameplay/GameControl.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour {
+
+    public enum Teams { team1, team2 };
+    public static GameControl instance;
+
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            Init();
+        } else
+        {
+            Destroy(this);
+        }
+    }
+
+
+    void Init()
+    {
+        SetupGameManagers();
+        InitControlSchemes();
+    }
+
+    void SetupGameManagers()
+    {
+        if (SettingsManager.instance == null)
+        {
+            gameObject.AddComponent<SettingsManager>();
+        }
+
+        if (AudioManager.instance == null)
+        {
+            gameObject.AddComponent<AudioManager>();
+        }
+    }
+
+    void InitControlSchemes()
+    {
+        string platform;
+#if UNITY_EDITOR_WIN
+        platform = "Windows";
+#elif UNITY_EDITOR_OSX
+        platform = "OSX";
+#elif UNITY_STANDALONE_OSX
+        platform = "OSX";
+#elif UNITY_STANDALONE_WIN
+        platform = "Windows";
+#endif
+        InputManager.LoadControlSchemes(platform);
+        InputManager.SetControlScheme(InputManager.PlayerEnum.playerOne, InputManager.availableControls[0]);
+        InputManager.SetControlScheme(InputManager.PlayerEnum.playerTwo, InputManager.availableControls[0]);
+        InputManager.SetControlScheme(InputManager.PlayerEnum.playerThree, InputManager.availableControls[0]);
+        InputManager.SetControlScheme(InputManager.PlayerEnum.playerFour, InputManager.availableControls[0]);
+    }
+
+    void Update()
+    {
+
+    }
+}
